9f4ededa51813e394d090c4c44f361cc47de6274
Vguilhen <vinicius.guilhen2013@gmail.com>
2022-07-07 11:14:50 -0300

    initialCommit

---
+++ app/src/main/java/com/example/vguilhen/affirmations/model/Affirmation.kt
@@ -0,0 +1,10 @@
+package com.example.vguilhen.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int ) {
+
+}
